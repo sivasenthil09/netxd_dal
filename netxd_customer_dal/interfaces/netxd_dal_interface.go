528cab8c35a4403a1a9430644c68c3ac088e40ed
sivasenthil09 <109544964+sivasenthil09@users.noreply.github.com>
2023-08-31 12:42:34 +0530

    Delete netxd_customer_dal directory

--- netxd_customer_dal/interfaces/netxd_dal_interface.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "github.com/sivasenthil09/netxd_dal/netxd_customer_dal/models"
-
-type ICustomer interface {
-	CreateCustomer(customer *models.Customer) (*models.DBResponse, error)
-}
